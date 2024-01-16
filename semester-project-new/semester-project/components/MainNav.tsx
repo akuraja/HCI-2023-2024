a825ab78bba33c55e0745cfdc7ce386ab5f3b330
akuraja <akuraj00@fesb.hr>
2024-01-16 14:53:20 +0100

    try3

--- semester-project-new/semester-project/components/MainNav.tsx
+++ semester-project-new/semester-project/components/MainNav.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Page } from "@/components/NavBar";
+import { Page } from "@/components/Navbar";
 
 const MainNav = ({ pages }: { pages: Page[] }) => {
   const pathname = usePathname();
